cc773acb4b2ce96ab8105c9b780e49270a74d919
EyeMLeo <Leonid.Akuta@gmail.com>
2022-11-09 16:39:40 +0100

    mini store done

--- js/posts.js
+++ js/posts.js
@@ -52,3 +52,56 @@ pr1()
 
 // 1 iskviesti getPosts tik po to kai sukurem posta su createPost su pavadinimu
 // 2 iskviesti getPosts tik po to kai sukurem posta su createPost su callback fn
+let postsDivEl = document.getElementById("cards");
+let postsDivEl2 = document.getElementById("cards2");
+
+function fetchData(url, howMany, target, root) {
+  fetch(url)
+    .then((arrPakas) => arrPakas.json())
+    .then((ArrPackasReadable) => {
+      for (let index = 0; index < howMany; index++) {
+        let element = "";
+        root === 1
+          ? (element = ArrPackasReadable.posts[index])
+          : (element = ArrPackasReadable[index]);
+
+        let oneCard = `  <div class="card">
+                  <h2>${element.title}</h2>
+                  <p>${element.body}</p>
+                </div>`;
+        target.insertAdjacentHTML("afterbegin", oneCard);
+      }
+    });
+}
+fetchData("https://jsonplaceholder.typicode.com/posts", 5, postsDivEl);
+
+// 1. is gautu duomenu atvaizduoti sarasa su vardais ir tel numeriais
+// 2. post.js faile panaudoti Promise vietoj callback
+// 3. parsisiusti postus is https://jsonplaceholder.typicode.com/posts su fetch
+// 4. pasilikti tik pirmus 10.
+// 5. is tu desimties sugeneruoti korteles htmle.
+// 6. parsiusti postus is https://dummyjson.com/posts
+
+// 7. sugeneruoti juos koteliu pavidale htmle.
+fetchData("https://dummyjson.com/posts", 15, postsDivEl2, 1);
+// 8. parasyti funkcija kuria iskvietus su fetch parsiunciami ir iskonsolinami produktai is https://dummyjson.com/products
+function fetchDataAdvancet(url, howMany, target, root) {
+  fetch(url)
+    .then((arrPakas) => arrPakas.json())
+    .then((ArrPackasReadable) => {
+      for (let index = 0; index < howMany; index++) {
+        let element = ArrPackasReadable.products[index];
+
+        let oneCard = `  <div class="card">
+<img src="${element.images[0]}">
+                  <h2>${element.title}</h2>
+                  <p>${element.description}</p>
+                </div>`;
+        target.insertAdjacentHTML("afterbegin", oneCard);
+      }
+    });
+}
+let phonesEl = document.getElementById("phones");
+fetchDataAdvancet("https://dummyjson.com/products", 15, phonesEl);
+// 9. parasyti funkcija kuri gauna masyva prekiu is 8uzd. ir juos avaizduoja html sarase.
+// 10. panaudoti 8uzd ir 9uzd funkcijas ir atvaizduoti prekes
